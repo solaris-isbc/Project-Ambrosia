70e7627ad5c8c0c5545d87e104d9113a7a40b9cb
Leonhard Kunz <solaris@interstellarbroadcasting.center>
2024-03-09 17:45:44 +0100

    created basic models

--- backend/api/ambrosia/models.py
+++ backend/api/ambrosia/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+
+    name = models.CharField(
+        max_length=250,
+        blank=False,
+        null=False
+    )
+
+    steps = models.TextField(
+        blank=False,
+        null=False
+    )
+
+    duration = models.IntegerField(
+        null=True
+    )
+
+    # TODO: categories, pictures, amount of people
+
+
+class Unit(models.Model):
+
+    name = models.CharField(
+        max_length=250,
+        blank=False,
+        null=False
+    )
+
+    abbreviation = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True
+    )
+
+
+class Ingredient(models.Model):
+
+    name = models.CharField(
+        max_length=250,
+        blank=False,
+        null=False
+    )
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        null=False
+    )
+
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        null=False
+    )
+
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    amount = models.FloatField(
+        null=True
+    )
+
